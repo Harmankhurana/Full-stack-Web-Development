7a7da0389e5f037a8cb5572d7729544e1ff20a5f
Harman khurana <work.harmankhurana@gmail.com>
2025-08-10 11:47:42 +0530

    DOM

---
+++ 16-DOM/index.js
@@ -0,0 +1,18 @@
+// Different DOM code to select different elements
+// document.querySelector();
+// document.querySelectorAll();
+// document.getElementById();
+// document.getElementsByClassName();
+
+
+// Changing the color of button using DOM 
+document.querySelector("button").style.backgroundColor = "yellow";
+
+// Adding a class name in a class element 
+document.querySelector("button").classList.add("invisible");
+
+// Removing a class name in a class element 
+document.querySelector("button").classList.remove("invisible");
+
+// changing the innerHTML of H1 element
+document.querySelector("h1").innerHTML = "Heyy there";
