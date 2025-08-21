c61fcd088a6eece14adbd8b20c2d80f80fb84a90
Harman khurana <work.harmankhurana@gmail.com>
2025-08-21 23:20:58 +0530

    jQuery Introduction

---
+++ 19-jQuery/jQuery.js
@@ -0,0 +1,11 @@
+// jQuery is a fast, small, and feature-rich JavaScript library. Its primary purpose is to simplify client-side scripting of HTML, 
+// making tasks like HTML document traversal and manipulation, event handling, animation, and Ajax interactions much simpler.
+
+// In JS for selecting Something we use 
+document.querySelector("h1");
+
+// But in jQuery we just write 
+jQuery("h1");
+
+// And for more short 
+$("h1");
