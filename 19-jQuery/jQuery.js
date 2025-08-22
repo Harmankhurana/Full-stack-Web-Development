1b6262288df10992223e8033ddb3f09c8ca7242d
Harman khurana <work.harmankhurana@gmail.com>
2025-08-22 09:33:45 +0530

    jQuery

--- 19-jQuery/jQuery.js
+++ 19-jQuery/jQuery.js
@@ -1,6 +1,5 @@
 // jQuery is a fast, small, and feature-rich JavaScript library. Its primary purpose is to simplify client-side scripting of HTML, 
 // making tasks like HTML document traversal and manipulation, event handling, animation, and Ajax interactions much simpler.
-
 // In JS for selecting Something we use 
 document.querySelector("h1");
 
@@ -8,7 +7,19 @@ document.querySelector("h1");
 jQuery("h1");
 
 // And for more short 
-$("h1");
+$("h1").css("color" , "red");
 
+// For selecting button element 
+document.querySelector("button");
+// Or 
+$("button");
 
-alert("Hello");
+// Manipulating Text with jQuery 
+// Changing Hello to Good Bye
+$("h1").text("Good Bye");      
+
+// Including Heyyy text in the button
+$("button").text("Heyyy");
+
+// Manipulating Attributes with jQuery 
+
