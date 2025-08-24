eff36b26e756a00b301402b2f69b5768781f941a
Harman khurana <work.harmankhurana@gmail.com>
2025-08-24 16:30:34 +0530

    Bash Commands

---
+++ 21-The-Unix-Command/command..js
@@ -0,0 +1,28 @@
+// What is Unix Command Line??
+// The Unix command line is the text-based interface you use to interact with a Unix-like operating system
+
+// What is Shell 
+// A shell is a program that acts as an interface between the user and the operating system.
+
+// There are Two types of Interface - GUI and CLI 
+// We will be using Bash shell
+// Bash shell is a CLI for the Unix system 
+// to install git bash - https://git-scm.com/downloads
+// CL to be dangerous - https://www.learnenough.com/command-line-tutorial
+
+// The Commands - 
+
+ls       // List all the folders and files
+cd       // Change directory
+pwd      // Show current working directory path
+mkdir    // Create a new folder (directory)
+touch    // Create a new empty file
+rm       // Remove a file
+rm -r    // Remove a folder (directory) and its contents
+cp       // Copy a file
+mv       // Move or rename a file/folder
+cat      // Show contents of a file
+clear    // Clear the terminal screen
+echo     // Print text to the terminal
+history  // Show previously entered commands
+exit     // Close the terminal
