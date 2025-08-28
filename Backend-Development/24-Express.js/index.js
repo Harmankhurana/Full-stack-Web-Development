62196eb15493c4dfbf825466b62de78f6a60adb9
Harman khurana <work.harmankhurana@gmail.com>
2025-08-29 00:52:07 +0530

    Express.js

--- Backend-Development/24-Express.js/index.js
+++ Backend-Development/24-Express.js/index.js
@@ -1,11 +1,30 @@
 // Creating a backend server using Node.js and Express.js
-const express = require("express");
+// const express = require("express");
+// OR
+import express from "express"; 
 const app = express();
 
-app.get("/" , (req , res) =>{
+app.get("/hello" , (req , res) =>{
     res.send({
-        "message" : "Hello I'm in the server",
+        message: "Hello I'm in the server",
     })
 })
 
-app.listen(3000);
+app.get("/contact" , (req , res) =>{
+    res.send({
+        message : [    // Making an array
+            "harmankhurana19@gmail.com",
+            "7357304047"
+        ]
+    })
+})
+
+app.get("/about" , (req , res) =>{
+    res.send({
+        message : "I'm a Software Engineer",
+    })
+})
+
+app.listen(3000 , () =>{
+    console.log("Server started on port 3000");  // this can be seen in termainal 
+});
