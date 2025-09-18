89513e8182501490d28b44b1f3ae73ee2ec3fed1
Harman khurana <work.harmankhurana@gmail.com>
2025-09-19 01:30:55 +0530

    Middleware

--- Backend-Development/24-Express.js/Middleware/index2.js
+++ Backend-Development/24-Express.js/Middleware/index2.js
@@ -1,7 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
+
+app.use(morgan("tiny"));
+// tiny is used for less info
+// Combined tells more information about the HTTP request
 
 app.get("/", (req, res) => {
   res.send("Hello");
