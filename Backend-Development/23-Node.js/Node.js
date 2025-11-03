76a7d168aa2f884bf94070422c30a11c8cce81c0
Harman khurana <work.harmankhurana@gmail.com>
2025-11-03 11:36:50 +0530

    Node.js

--- Backend-Development/23-Node.js/Node.js
+++ Backend-Development/23-Node.js/Node.js
@@ -1,12 +1,12 @@
 // https://nodejs.org/en
 // Using a Native Node module, which is File(fs) - writing and reading 
-const fs = require("fs");
+
+import fs from "fs";
 
 fs.writeFile("node.txt" , "Hello from NodeJS" , function(err , data){
     if(err) throw err;
     console.log("The file has been saved!");
 });
-
 
 fs.readFile("./Node.txt" , "utf-8" , function(err, data){
     if(err) throw err; 
