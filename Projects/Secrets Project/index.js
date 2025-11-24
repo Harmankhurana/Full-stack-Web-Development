23273add898a127c9e57260905c4329a53a8a0ef
Harman khurana <work.harmankhurana@gmail.com>
2025-11-24 11:02:35 +0530

    Secrete Project

--- Projects/Secrets Project/index.js
+++ Projects/Secrets Project/index.js
@@ -1,43 +1,13 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
- 
-import express from "express";
-import bodyParser from "body-parser";
-import {dirname} from "path";
-import { fileURLToPath} from "url";
-const _dirname = dirname(fileURLToPath(import.meta.url));
+// HINTS:
+// 1. Import express and axios
 
-const app = express();
-const port = 3000;
+// 2. Create an express app and set the port number.
 
-let userIsAuthorised = false;
+// 3. Use the public folder for static files.
 
-app.use(bodyParser.urlencoded({ extended : true }));
+// 4. When the user goes to the home page it should render the index.ejs file.
 
-function passwordCheck(req , res , next){
-    const password = req.body["password"];
-    if (password === "ILoveProgramming"){
-        userIsAuthorised = true;
-    }
-    next();
-}
+// 5. Use axios to get a random secret and pass it to index.ejs to display the
+// secret and the username of the secret.
 
-app.use(passwordCheck);
-
-app.get("/" , (req , res) => {
-    res.sendFile(_dirname + "/public/index.html");
-});
-
-app.post("/check" , (req , res) => {
-    if(userIsAuthorised){
-        res.sendFile(_dirname + "/public/secret.html");
-    }
-    else{
-        res.sendFile(_dirname + "/public/index.html");
-    }
-})
-
-app.listen(port , () => {
-    console.log(`Listening on port ${port}`);
-})
+// 6. Listen on your predefined port and start the server.
--- Projects/Secrets Project/solution.js
+++ Projects/Secrets Project/solution.js
@@ -1,38 +1,24 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import axios from "axios";
 
 const app = express();
 const port = 3000;
 
-var userIsAuthorised = false;
+app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-function passwordCheck(req, res, next) {
-  const password = req.body["password"];
-  if (password === "ILoveProgramming") {
-    userIsAuthorised = true;
-  }
-  next();
-}
-app.use(passwordCheck);
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-app.post("/check", (req, res) => {
-  if (userIsAuthorised) {
-    res.sendFile(__dirname + "/public/secret.html");
-  } else {
-    res.sendFile(__dirname + "/public/index.html");
-    //Alternatively res.redirect("/");
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get("https://secrets-api.appbrewery.com/random");
+    res.render("index.ejs", {
+      secret: result.data.secret,
+      user: result.data.username,
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
   }
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
