8229c1907f3fa1e51bf663137861364ebb047d53
Harman khurana <work.harmankhurana@gmail.com>
2026-01-14 23:27:35 +0530

    Learning about API requests

---
+++ Backend-Development/28-API/API Requests/index.js
@@ -0,0 +1,10 @@
+// working on sending postman requests to a URL (https://bored-api.appbrewery.com/)
+
+// Make an API request to get random activity
+// https:/bored-api.appbrewery.com/random
+
+// Make an API request through postman that queries all the "Socail" activities for 2 people
+// https:/bored-api.appbrewery.com/filter?type=social&participants=2;
+
+// Make an API request through postman that has a specific path parameter
+// https:/bored-api.appbrewery.com/activity/3943506
